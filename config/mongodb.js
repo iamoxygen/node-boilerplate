101b6ec8560a80db7246c0f12558c759537fae59
iamoxygen <vikash.chaubey0.vc@gmail.com>
2024-11-08 01:36:06 +0530

    create boilerplate

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-const { uri, title } = require("../helper/environment");
+const environment = require("../helper/environment");
 const { logger } = require("../helper/ErrorHandler");
 
 (function () {
     try {
         mongoose
-            .connect(uri ?? "")
+            .connect(environment?.uri ?? "")
             .then((value) =>
-                console.log(`${title ?? "unknown"} db is connected successfully`)
+                console.log(`${environment?.title ?? "unknown"} db is connected successfully`)
             )
             .catch((value) => console.log(`Db connection error`, value));
     } catch (error) {
